f15de9a980a904e9a88e45410d3a7013692f08a8
ShimonShoji <shimon.shoji@gmail.com>
2022-01-02 15:51:17 +0000

    Update

--- scripts/hand.py
+++ scripts/hand.py
@@ -3,18 +3,64 @@ import rospy
 from std_msgs.msg import Int32
 
 n = 0
-dice[]=[0,0,0,0,0]
+dice = [0,0,0,0,0,0,100]
 
 def cb(message):
-	global n
-	n = message.data
+    global n
+    n = message.data
+    if dice[4] != 0:
+        dice[5] = n
+        rospy.loginfo(dice[5]*100)
+
+        #ソート
+        #判定
+
+    elif dice[3] != 0:
+        dice[4] = n
+        rospy.loginfo(dice[4]*100)
+
+    elif dice[2] != 0:
+        dice[3] = n
+        rospy.loginfo(dice[3]*100)
+
+    elif dice[1] != 0:
+        dice[2] = n
+        rospy.loginfo(dice[2]*100)
+
+    else:
+        dice[1] = n
+        rospy.loginfo(dice[1]*100)
 
 rospy.init_node('hand')
 sub = rospy.Subscriber('roll', Int32, cb)
 
-while not rospy.is_shutdown():
-	dice[0]=n
-	rospy.loginfo(dice[0]*100)
-	rate.sleep
+"""
+if dice[4] != 0:
+    dice[5] = n
+    rospy.loginfo(dice[5]*100)
 
+elif dice[3] != 0:
+    dice[4] = n
+    rospy.loginfo(dice[4]*100)
+
+elif dice[2] != 0:
+    dice[3] = n
+    rospy.loginfo(dice[3]*100)
+
+elif dice[1] != 0:
+    dice[2] = n
+    rospy.loginfo(dice[2]*100)
+
+else:
+    dice[1] = n
+
+    rospy.loginfo(dice[0]*100)
+    rospy.loginfo(dice[1]*100)
+    rospy.loginfo(dice[2]*100)
+    rospy.loginfo(dice[3]*100)
+    rospy.loginfo(dice[4]*100)
+    rospy.loginfo(dice[5]*100)
+    rospy.loginfo(dice[6]*100)
+"""
 rospy.spin()
+
